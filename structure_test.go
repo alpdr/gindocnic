a4595a52a0a243bd362603c58da44373ded28b3b
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2026-01-21 23:21:36 +0900

    test: add tests for converting struct to JSON schema

--- structure_test.go
+++ structure_test.go
@@ -1,6 +1,7 @@
 package gindocnic
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -44,4 +45,75 @@ func TestConvertStruct(t *testing.T) {
 			tt.assertion(res, err)
 		})
 	}
+}
+
+func TestGinStructToJsonSchemaGo(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name     string
+		input    any
+		expected any
+	}{
+		{
+			name: "Make uri path",
+			input: struct {
+				ID string `uri:"id"`
+			}{},
+			expected: struct {
+				ID int `path:"id"`
+			}{},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+			actual, err := convertStruct(testCase.input, nil, nil)
+			if err != nil {
+				t.Errorf("unexpected error: %#v", err)
+			}
+			if err := checkEqual(t, testCase.expected, actual); err != nil {
+				t.Error(err)
+			}
+
+		})
+	}
+
+}
+func checkEqual(t *testing.T, expected, actual any) error {
+	t.Helper()
+
+	expectedVal := reflect.ValueOf(expected)
+	actualVal := reflect.ValueOf(actual)
+	if expectedVal.Kind() != actualVal.Kind() {
+		return fmt.Errorf("kind mismatch: expected %s, got %s", expectedVal.Kind(), actualVal.Kind())
+	}
+
+	if expectedVal.Kind() == reflect.Struct {
+		return checkStructEqual(t, expected, actual)
+	}
+
+	panic("not implemented")
+}
+
+func checkStructEqual(t *testing.T, expected, actual any) error {
+	t.Helper()
+	expectedType := reflect.TypeOf(expected)
+	actualType := reflect.TypeOf(actual)
+
+	v := reflect.ValueOf(expected)
+	v.Interface()
+
+	numFields := expectedType.NumField()
+	if numFields != actualType.NumField() {
+		return fmt.Errorf("field count mismatch: expected %d, got %d", expectedType.NumField(), actualType.NumField())
+	}
+
+	for i := range numFields {
+		expectedField := expectedType.Field(i)
+		actualField := actualType.Field(i)
+
+	}
+
 }
